350b2d5990d6467bbc723bd40e794c6736ed64d6
arjun98k <arjunkandekar14@gmail.com>
2025-03-11 10:59:38 +0530

    collection

---
+++ col1.java
@@ -0,0 +1,17 @@
+
+
+
+import java.util.ArrayList;
+
+public class col1 {
+   
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(11);
+        list1.add(12);
+        list1.add(13);
+        list1.add(14);
+        System.out.println(list1);
+    }
+}
