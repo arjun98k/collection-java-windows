ce7ae68a0351913022ff69039337bed9bcbe0753
arjun98k <arjunkandekar14@gmail.com>
2025-03-11 17:40:06 +0530

    pass by value and pass by reference

--- arr1/Reference1.java
+++ arr1/Reference1.java
@@ -1,7 +1,26 @@
 package arr1;
 
+class Student{
+    public  int id ;
+}
+
 public class Reference1 {
     public static void main(String[] args) {
-        System.out.println("arjun");
+    Student student = new Student();
+    student.id = 1;
+    fun(student);
+        System.out.println(student.id);
+
+
+
+
+    }
+
+    private static  void fun(Student a){
+        Student student = new Student();
+        student.id = 2;
+        a = student;
+        System.out.println("change in fun method" +" " +  a.id);
+        a.id = 2;
     }
 }
