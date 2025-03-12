0b7829addce9bdf84993a496bf5cc48598f9c4f1
arjun98k <arjunkandekar14@gmail.com>
2025-03-12 23:52:25 +0530

    enumeration

---
+++ enumeration/Enum1.java
@@ -0,0 +1,8 @@
+package enumeration;
+
+public class Enum1
+{
+    public static void main(String[] args) {
+        System.out.println(Enumclass.FRIDAY);
+    }
+}
---
+++ enumeration/Enumclass.java
@@ -0,0 +1,5 @@
+package enumeration;
+
+public enum Enumclass {
+    SUNDAY , MONDAY , TUESDAY , WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
