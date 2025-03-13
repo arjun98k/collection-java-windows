d1822352d7b81b89fb0f5b2b24e422533ac964a2
arjun98k <arjunkandekar14@gmail.com>
2025-03-13 13:27:20 +0530

    generics concept

--- genericspack/Genricstud1.java
+++ genericspack/Genricstud1.java
@@ -12,10 +12,31 @@ class Box {
     }
 }
 
+
+class  Pair<K,V> {
+    private K key;
+    private V value;
+
+    public Pair(K key , V value){
+        this.key = key;
+        this.value = value;
+    }
+    public K getKey(){
+        return key;
+    }
+    public V getValue(){
+        return value;
+    }
+}
+
 public class Genricstud1 {
     public static void main(String[] args) {
      Box ak = new Box();
-     ak.setValue(11);
-        System.out.println(ak.getValue());
+//     ak.setValue(11);
+//        System.out.println(ak.getValue());
+
+        Pair<String, Integer> pair = new Pair<>("Arjun", 98);
+        System.out.println(pair.getKey());
+        System.out.println(pair.getValue());
     }
 }
