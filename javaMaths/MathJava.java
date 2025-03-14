573ed662e8519dbf8e5397bd74163e78b1067272
arjun98k <arjunkandekar14@gmail.com>
2025-03-14 18:39:09 +0530

    Math java fun

--- javaMaths/MathJava.java
+++ javaMaths/MathJava.java
@@ -4,5 +4,6 @@ public class MathJava {
     public static void main(String[] args) {
         System.out.println(" is return absloute number  " + Math.abs(-11));
         System.out.println("it return high value " +  Math.max(98,11));
+        System.out.println("" + Math.addExact((int) 11.2, (int) 33.6));
     }
 }
