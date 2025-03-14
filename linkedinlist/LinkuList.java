48def30336c88fc4aa10dd9fc7d3c594761ccc35
arjun98k <arjunkandekar14@gmail.com>
2025-03-14 13:23:18 +0530

    Linledinlist

---
+++ linkedinlist/LinkuList.java
@@ -0,0 +1,18 @@
+package linkedinlist;
+
+import java.util.LinkedList;
+
+
+
+public class LinkuList {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        l1.add(11);
+        l1.add(2);
+        l1.add(3);
+       for(int element : l1){
+           System.out.println(element);
+       }
+    }
+}
