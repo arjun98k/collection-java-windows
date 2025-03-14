3532b8083cbed9cb589059038e389fc7d7bc3e6d
arjun98k <arjunkandekar14@gmail.com>
2025-03-14 18:32:01 +0530

    Math java fun

--- javaMaths/MathJava.java
+++ javaMaths/MathJava.java
@@ -2,6 +2,7 @@ package javaMaths;
 
 public class MathJava {
     public static void main(String[] args) {
-        System.out.println(" is return absloute number  " + Math.abs(-98));
+        System.out.println(" is return absloute number  " + Math.abs(-11));
+        System.out.println("it return high value " +  Math.max(98,11));
     }
 }
