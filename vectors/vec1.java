d10142444d4f12ea4e50a0ec42de8d517a2d1f96
arjun98k <arjunkandekar14@gmail.com>
2025-03-15 16:51:03 +0530

    vector in cf

--- vectors/vec1.java
+++ vectors/vec1.java
@@ -4,12 +4,16 @@ import java.util.Vector;
 
 public class vec1 {
     public static void main(String[] args) {
-        Vector l1 = new Vector<>();
+        Vector l1 = new Vector<>(5,4);
         l1.add(1);
         l1.add(2);
         l1.add(3);
         l1.add(4);
         l1.add(5);
+        System.out.println(l1);
+        for (int i = 1 ; i < 5; i++){
+            l1.add(i);
+        }
         System.out.println(l1);
         System.out.println(l1.capacity());
     }
