4880cdf92f3e8841d0d80d3e0c37aad46e7fc4a1
arjun98k <arjunkandekar14@gmail.com>
2025-03-16 19:17:05 +0530

    hashmap methods in cf

--- hashMap/Hashmap1.java
+++ hashMap/Hashmap1.java
@@ -1,6 +1,7 @@
 package hashMap;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Hashmap1 {
     public static void main(String[] args) {
@@ -9,6 +10,12 @@ public class Hashmap1 {
         l1.put(2,"amey");
         l1.put(3,"raju");
         l1.put(4,"Arjun");
-        System.out.println(l1);
+//        System.out.println(l1);
+        System.out.println( l1.containsValue("Arjun"));
+        System.out.println(l1.get(3));
+//        for (Map.Entry<Integer, String> entry : l1.entrySet()) {
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
+
     }
 }
