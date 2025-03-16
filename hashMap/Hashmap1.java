308f7577808159f61f722d2b5709d181a3710e0b
arjun98k <arjunkandekar14@gmail.com>
2025-03-16 18:58:03 +0530

    hashmap methods in cf

---
+++ hashMap/Hashmap1.java
@@ -0,0 +1,14 @@
+package hashMap;
+
+import java.util.HashMap;
+
+public class Hashmap1 {
+    public static void main(String[] args) {
+        HashMap<Integer,String> l1 = new HashMap<>();
+        l1.put(1, "aditi");
+        l1.put(2,"amey");
+        l1.put(3,"raju");
+        l1.put(4,"Arjun");
+        System.out.println(l1);
+    }
+}
