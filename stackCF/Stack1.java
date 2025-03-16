8ad2cafc34600a928c7446d680fb1021e24ac6b7
arjun98k <arjunkandekar14@gmail.com>
2025-03-16 15:34:16 +0530

    stack operation

--- stackCF/Stack1.java
+++ stackCF/Stack1.java
@@ -14,8 +14,8 @@ public class Stack1 {
 //        System.out.println(list.peek());
 //        System.out.println(list.pop());
 
-        System.out.println(list.getFirst());
-
+//        System.out.println(list.getFirst());
+        System.out.println( list.search(3));
 //        for (int elements: list){
 //            System.out.println(elements);
 //        }
