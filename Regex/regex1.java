4cbf61c88bbc669f02874e8bef8b44a87b4b169d
arjun98k <arjunkandekar14@gmail.com>
2025-03-25 17:39:16 +0530

    regex ecpression

--- Regex/regex1.java
+++ Regex/regex1.java
@@ -5,8 +5,10 @@ import java.util.regex.Pattern;
 
 public class regex1 {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("arjun");
-        Matcher matcher = pattern.matcher("ar");
+        Pattern pattern = Pattern.compile("a*b");
+                                          // * mean more a or zero a and last mai b hona chahiye
+                                           // b ke baad be star ho toh utne bar b chahiye
+        Matcher matcher = pattern.matcher("ba");
 
         boolean matches = matcher.matches();
         System.out.println(matches);
