b9c12f2cb0e0201dbaf7fc79bacc3d9cfd6310fa
arjun98k <arjunkandekar14@gmail.com>
2025-03-25 17:41:30 +0530

    regex ecpression

--- Regex/regex1.java
+++ Regex/regex1.java
@@ -6,9 +6,9 @@ import java.util.regex.Pattern;
 public class regex1 {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("a*b");
-                                          // * mean more a or zero a and last mai b hona chahiye
-                                           // b ke baad be star ho toh utne bar b chahiye
-        Matcher matcher = pattern.matcher("ba");
+                                          // * mean more a or zero or usse jyaada bhi ho chalega a and last mai b hona chahiye
+
+        Matcher matcher = pattern.matcher("aaaabbb");
 
         boolean matches = matcher.matches();
         System.out.println(matches);
