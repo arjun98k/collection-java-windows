b924e84579af2ae2c810e628a0722026968e03f1
arjun98k <arjunkandekar14@gmail.com>
2025-03-28 17:24:31 +0530

    throws from main exception

--- ExceptionHnadling/Example2.java
+++ ExceptionHnadling/Example2.java
@@ -4,7 +4,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Example2 {
-    public static void main(String[] args) throws FileNotFoundException {
-        FileReader fileread = new FileReader("a.txt");
+//    public static void main(String[] args) throws FileNotFoundException {
+//        FileReader fileread = new FileReader("a.txt");
+
+    public static void main(String[] args) {
+       try {
+           int result = 10/0;
+       } catch (Exception e) {
+           System.out.println("catched error");
+       }finally {
+           System.out.println("finally block inside");
+       }
+
+
     }
 }
