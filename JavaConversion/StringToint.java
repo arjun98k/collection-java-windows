bf329f5d17b49cbf12bc54cdc3d34881ea0c05d5
arjun98k <arjunkandekar14@gmail.com>
2025-03-29 23:55:16 +0530

    convert string to int

---
+++ JavaConversion/StringToint.java
@@ -0,0 +1,9 @@
+package JavaConversion;
+
+public class StringToint {
+    public static void main(String[] args) {
+        String num = "123";
+        int b = Integer.parseInt(num);
+        System.out.println(num.getClass().getSimpleName());
+    }
+}
