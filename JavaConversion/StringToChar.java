759f02a7db1c7cbceb41241d810eba672bdb1198
arjun98k <arjunkandekar14@gmail.com>
2025-03-30 10:56:38 +0530

    String to char

---
+++ JavaConversion/StringToChar.java
@@ -0,0 +1,10 @@
+package JavaConversion;
+
+public class StringToChar {
+    public static void main(String[] args) {
+        String str = "Arjun";
+
+        char c = str.charAt(0);
+//        System.out.println(c);
+    }
+}
