1a9eba3c5b42924f65ed774b771b0e6b3cce227d
arjun98k <arjunkandekar14@gmail.com>
2025-03-30 12:07:48 +0530

    char to string

---
+++ JavaConversion/CharToString.java
@@ -0,0 +1,10 @@
+package JavaConversion;
+
+public class CharToString {
+    public static void main(String[] args) {
+        String str = "a";
+       char bk = str.charAt(0);
+        System.out.println(bk);
+
+    }
+}
