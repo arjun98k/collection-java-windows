d0405f8b467f3cb0cb30101084b93fe1efe9dcdb
arjun98k <arjunkandekar14@gmail.com>
2025-04-07 19:50:52 +0530

    identity hashmap

--- GC/IdentityHashmap.java
+++ GC/IdentityHashmap.java
@@ -1,13 +1,14 @@
 package GC;
 
 import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class IdentityHashmap {
     public static void main(String[] args) {
     String key1 = new String("key");
     String key2 = new String("key");
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new IdentityHashMap<>();
         map.put(key1,1);
         map.put(key2,2);
         System.out.println(map);
