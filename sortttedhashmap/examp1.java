173a14bc7e0cd11ca729ebc233acf2bf5d60ec87
arjun98k <arjunkandekar14@gmail.com>
2025-04-10 20:39:12 +0530

    sortedhashmap

---
+++ sortttedhashmap/examp1.java
@@ -0,0 +1,20 @@
+package sortttedhashmap;
+
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class examp1 {
+
+    public static void main(String[] args) {
+        SortedMap<String, Integer> sortermap = new TreeMap<>();
+        sortermap.put("aditya",11);
+        sortermap.put("arjun",98);
+        sortermap.put("cutie",99);
+        sortermap.put("kritika",101);
+
+        for (Map.Entry<String,Integer> entry: sortermap.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
