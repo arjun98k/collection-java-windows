8940b515b40f04bdd6da2e291bb0e7dfbf4dfb60
arjun98k <arjunkandekar14@gmail.com>
2025-04-11 13:05:47 +0530

    sorted hashashmap

--- sortttedhashmap/examp1.java
+++ sortttedhashmap/examp1.java
@@ -12,9 +12,10 @@ public class examp1 {
         sortermap.put("arjun",98);
         sortermap.put("cutie",99);
         sortermap.put("kritika",101);
+        System.out.println(sortermap);
 
-        for (Map.Entry<String,Integer> entry: sortermap.entrySet()){
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
+//        for (Map.Entry<String,Integer> entry: sortermap.entrySet()){
+//            System.out.println(entry.getKey() + " " + entry.getValue());
+//        }
     }
 }
