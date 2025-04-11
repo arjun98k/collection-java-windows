7e8fd0484eaa4116a223184d6d211d0c878a1abc
arjun98k <arjunkandekar14@gmail.com>
2025-04-12 00:14:54 +0530

    linkedhash set

---
+++ set/linkedhashset.java
@@ -0,0 +1,14 @@
+package set;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class linkedhashset {
+    public static void main(String[] args) {
+        Set<Integer> set2 = new LinkedHashSet<>();
+        set2.add(12);
+        set2.add(13);
+        set2.add(14);
+        System.out.println(set2);
+    }
+}
