82f636f2d1475bae8c5f26295b77a581804511fb
arjun98k <arjunkandekar14@gmail.com>
2025-04-11 23:54:32 +0530

    set

---
+++ set/exmp1.java
@@ -0,0 +1,13 @@
+package set;
+
+import java.util.HashSet;
+
+public class exmp1 {
+    public static void main(String[] args) {
+        HashSet<Integer> set1 = new HashSet<>();
+        set1.add(11);
+        set1.add(12);
+        set1.add(11);
+        System.out.println(set1);
+    }
+}
