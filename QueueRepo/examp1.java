21f4704b7efaca6e72ca4e04d925e0f92ce6a3f8
arjun98k <arjunkandekar14@gmail.com>
2025-04-12 12:54:23 +0530

    Queue

---
+++ QueueRepo/examp1.java
@@ -0,0 +1,16 @@
+package QueueRepo;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class examp1 {
+    public static void main(String[] args) {
+        Queue<Integer> l1 = new LinkedList<>();
+        l1.add(11);
+        l1.add(12);
+        l1.add(13);
+        System.out.println(l1.peek());
+        System.out.println(l1.poll());
+        System.out.println(l1.offer(12));
+    }
+}
