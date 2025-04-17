b39871c62d3e78a956493c49a0f9416d10f4ce31
arjun98k <arjunkandekar14@gmail.com>
2025-04-17 12:57:05 +0530

    Thread life cycle

---
+++ MultiThread/Basic/Threadd1/Thread1.java
@@ -0,0 +1,18 @@
+package MultiThread.Basic.Threadd1;
+
+public class Thread1 extends Thread {
+    @Override
+    public void run() {
+        System.out.println("something happening 2nd thread "+ Thread.currentThread().getName() );
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        System.out.println(t1.getState());
+    }
+}
