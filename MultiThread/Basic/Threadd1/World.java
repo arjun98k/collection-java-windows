cb36715643372faef8f74aa3745a5f1c39553d62
arjun98k <arjunkandekar14@gmail.com>
2025-04-17 12:07:38 +0530

    create thread using thread class direct and rummable

--- MultiThread/Basic/Threadd1/World.java
+++ MultiThread/Basic/Threadd1/World.java
@@ -1,10 +1,19 @@
 package MultiThread.Basic.Threadd1;
 
-public class World extends Thread {
-    @Override
-    public void run() {
-        for (int i = 0; i <10000 ; i++) {
-            System.out.println("world of arjun " + Thread.currentThread().getName());
+//public class World extends Thread {
+//    @Override
+//    public void run() {
+//        for (int i = 0; i <10000 ; i++) {
+//            System.out.println("world of arjun " + Thread.currentThread().getName());
+//        }
+//    }
+//}
+
+public class World implements Runnable {
+
+    public void run(){
+        for (int i = 0; i < 10000 ; i++) {
+            System.out.println("world of 2 arjun  "+ Thread.currentThread().getName() );
         }
     }
 }
--- MultiThread/Basic/Threadd1/example.java
+++ MultiThread/Basic/Threadd1/example.java
@@ -2,8 +2,13 @@ package MultiThread.Basic.Threadd1;
 
 public class example {
     public static void main(String[] args) {
+//        through thread class
+//        World word = new World();
+//        word.start();
+
         World word = new World();
-        word.start();
+        Thread t1 = new Thread(word);
+                t1.start();
         for (int i = 0; i < 10000 ; i++) {
             System.out.println("hello bhau" +" "+ Thread.currentThread().getName());
         }
