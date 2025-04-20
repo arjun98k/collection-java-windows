64b4b7e3c3f81ab650ea47a6aa2654bf1ad6426b
arjun98k <arjunkandekar14@gmail.com>
2025-04-20 12:20:12 +0530

    Daemon thread

--- MultiThread/Basic/Threadd1/DaemonThread.java
+++ MultiThread/Basic/Threadd1/DaemonThread.java
@@ -9,7 +9,11 @@ public void run(){
 
     public static void main(String[] args) {
         DaemonThread t1 = new DaemonThread();
-         t1.setDaemon(true);
+        DaemonThread t2 = new DaemonThread();
+         t1.setDaemon(true); // DaemonThread is now daemonthread like (Garbage Collector) now
          t1.start();
+         t2.start();
+        System.out.println("main thread");
+
     }
 }
