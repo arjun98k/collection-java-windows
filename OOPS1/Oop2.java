c384ed99410e19faf588d9b9faa0392c7c85daad
arjun98k <arjunkandekar14@gmail.com>
2025-05-15 18:55:53 +0530

    anonymous

---
+++ OOPS1/Oop2.java
@@ -0,0 +1,14 @@
+package OOPS1;
+
+class OP1 {
+    void printMsg(){
+        System.out.println("good evening madam");
+    }
+}
+
+public class Oop2 {
+    public static void main(String[] args) {
+        new OP1().printMsg();
+    }
+}
+
