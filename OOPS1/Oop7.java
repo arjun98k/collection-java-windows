90fbabe64b969453ac486548742abd93560526cb
arjun98k <arjunkandekar14@gmail.com>
2025-05-17 01:53:44 +0530

    static variable imp

---
+++ OOPS1/Oop7.java
@@ -0,0 +1,22 @@
+package OOPS1;
+
+class Hp {
+    String name;
+    int id;
+    static String hosipitaltype = "ayurvedas";
+
+    Hp(String n, int I){
+        name = n;
+        id = I;
+    }
+    void show(){
+        System.out.println("name of hospital " + name + " id of hosiptal " + hosipitaltype );
+    }
+}
+
+public class Oop7 {
+    public static void main(String[] args) {
+   Hp obj = new Hp("moti",12);
+      obj.show();
+    }
+}
