6741354818cb2c8745501c5d428e3473cc7ecb8d
arjun98k <arjunkandekar14@gmail.com>
2025-06-04 23:58:36 +0530

    abstrat

---
+++ OOPS1/Oop11.java
@@ -0,0 +1,22 @@
+package OOPS1;
+
+
+ abstract class Adi{
+    void area(int a , int b){}
+}
+
+class Mani extends Adi {
+    @Override
+    void area(int a, int b) {
+
+      int c=  a * b;
+        System.out.println(c);
+    }
+}
+
+public class Oop11 {
+    public static void main(String[] args) {
+     Mani obj = new Mani();
+     obj.area(11,12);
+    }
+}
